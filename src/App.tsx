f4965a450952ce4c8587fa7292063b12f7788510
voucher153 <vouchercomm227@gmail.com>
2024-05-26 14:53:09 +0300

    setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { Route, Routes, useLocation } from 'react-router-dom';
